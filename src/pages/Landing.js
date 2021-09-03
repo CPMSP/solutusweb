c0735cf2a18c5fae8a0b8c60dcad4d11fca16527
C Peterson <cpetersonmsp@gmail.com>
2021-09-02 23:00:46 -0500

    settimeout on landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,23 +1,16 @@
-import React from "react";
-import { useStyles } from "../styles/landingStyles";
-import { Button } from "@material-ui/core";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { useStyles } from '../styles/landingStyles';
 
 export default function Landing() {
+	setTimeout(() => {
+		window.location.replace('/Main');
+	}, 2000);
 	const classes = useStyles();
 	return (
 		<div className={classes.landing}>
 			<div className={classes.marquee}>
 				<h1 className={classes.name}>SolutuS LLC</h1>
 				<h4 className={classes.purpose}>Mobile and Web Development</h4>
-				<Link exact to="/Main" style={{ textDecoration: "none" }}>
-					<Button
-						className={classes.button}
-						variant="outlined"
-						color="secondary">
-						Enter Site
-					</Button>
-				</Link>
 			</div>
 		</div>
 	);
